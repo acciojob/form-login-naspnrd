194bd5d3f62b6f9f36f02a70bc6c770753ca7567
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-16 21:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// function getFormvalue() {
+//   //Write your code here
+//   let inputs = document.querySelectorAll("input");
+//   console.log(inputs);
+//   let first_name = inputs[0].value;
+//   let last_name = inputs[1].value;
+//   let fullName = first_name + " " + last_name;
+//   alert(fullName);
+	
+// }
+
+
 function getFormvalue() {
-  //Write your code here
-  let inputs = document.querySelectorAll("input");
-  console.log(inputs);
-  let first_name = inputs[0].value;
-  let last_name = inputs[1].value;
+  let first_name = document.getElementById("fname").value
+  let last_name = document.getElementById("lname").value
   let fullName = first_name + " " + last_name;
   alert(fullName);
 	
